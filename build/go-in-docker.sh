752a1741483b3de535a24c2c5106b1a86efc4f4a
Kushal Kumaran <Kushal.Kumaran@veritas.com>
2020-02-04 16:35:27 -0800

    update e2e image
    This picks up commit 08e19a2788a491610adf0608e2780d5a6a11bf5c from
    upstream.

--- build/go-in-docker.sh
+++ build/go-in-docker.sh
@@ -40,7 +40,7 @@ if [ "$missing" = true ];then
   exit 1
 fi
 
-E2E_IMAGE=quay.io/kubernetes-ingress-controller/e2e:v04062019-3dc4253b7
+E2E_IMAGE=quay.io/kubernetes-ingress-controller/e2e:v02042020-08e19a278
 
 DOCKER_OPTS=${DOCKER_OPTS:-""}
 
